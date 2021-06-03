b42b819a61348ba8a7c3c969bf746bc1a4022eee
kibetbrian <kimutaibrian9@gmail.com>
2021-06-03 02:41:50 -0700

    Created Company Repository And Interface

---
+++ app/Invoicer/Repositories/Eloquent/CompanyRepository.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Invoicer\Repositories\Eloquent;
+
+use App\Invoicer\Repositories\Contracts\CompanyInterface;
+use App\Models\Company;
+
+class CompanyRepository extends BaseRepository implements CompanyInterface {
+
+    protected $model;
+
+    /**
+     * CompanyRepository constructor.
+     * @param Company $model
+     */
+    function __construct(Company $model)
+    {
+        $this->model = $model;
+    }
+
+}
