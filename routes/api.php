f06ac3686330c08d74f5a9d82e3d8716231cfe8f
Kevin <geykure@gmail.com>
2021-06-06 10:26:57 +0300

    Added routes protection

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
-use App\Http\Controllers\Api\GeneralSettingController;
+use App\Http\Controllers\Api\Oauth\LoginController;
+use App\Http\Controllers\Api\PermissionController;
+use App\Http\Controllers\Api\RoleController;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\CustomerController;
 use App\Http\Controllers\Api\ProductCategoryController;
@@ -12,7 +14,6 @@ use App\Http\Controllers\Api\ExpenseCategoryController;
 use App\Http\Controllers\Api\ExpenseController;
 use App\Http\Controllers\Api\PaymentController;
 use App\Http\Controllers\Api\CompanyController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,7 +26,31 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+
+/*
+|--------------------------------------------------------------------------
+| Authentication routes
+|--------------------------------------------------------------------------
+|
+|  Routes to obtain access_token and manage token refresh
+|
+*/
 Route::group(array('prefix' => '/v1'), function () {
+    Route::post('/login', [LoginController::class, 'login']);
+    Route::post('/login/refresh', [LoginController::class, 'refresh']);
+});
+
+/*
+|--------------------------------------------------------------------------
+| Protected system routes
+|--------------------------------------------------------------------------
+|
+| For both admin users and customers
+|
+*/
+Route::prefix('v1')->middleware(['auth:api,customers', 'throttle:60,1'])->group(function () {
+
     Route::resource('/users', UserController::class);
     Route::resource('/customers', CustomerController::class);
     Route::resource('/product/categories', ProductCategoryController::class);
@@ -38,6 +63,34 @@ Route::group(array('prefix' => '/v1'), function () {
     Route::resource('/payments', PaymentController::class);
     Route::resource('/companies', CompanyController::class);
 
-    Route::resource('general_settings', GeneralSettingController::class)->except(['create', 'edit'])
-        ->middleware(['scope:test1']);
+    Route::resource('roles', RoleController::class)->except(['create', 'edit']);
+    // ->middleware(['scope:test1']);
+
+    Route::resource('permissions', PermissionController::class)->except(['create', 'edit']);
+    // ->middleware(['scope:test1']);
+});
+
+
+
+// Unprotected routes
+// Dev only
+//TODO  - remove this section on production launch
+Route::group(array('prefix' => '/v0'), function () {
+    Route::resource('/users', UserController::class);
+    Route::resource('/customers', CustomerController::class);
+    Route::resource('/product/categories', ProductCategoryController::class);
+    Route::resource('/products', ProductController::class);
+    Route::resource('/currencies', CurrencyController::class);
+    Route::resource('/invoices', InvoiceController::class);
+    Route::resource('/quotations', QuotationController::class);
+    Route::resource('/expenses/categories', ExpenseCategoryController::class);
+    Route::resource('/expenses', ExpenseController::class);
+    Route::resource('/payments', PaymentController::class);
+    Route::resource('/companies', CompanyController::class);
+
+    Route::resource('roles', RoleController::class)->except(['create', 'edit']);
+       // ->middleware(['scope:test1']);
+
+    Route::resource('permissions', PermissionController::class)->except(['create', 'edit']);
+       // ->middleware(['scope:test1']);
 });
